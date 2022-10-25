c4b0758854da23f14b7b2812137fe5a1bf2af295
Jay Ponder <ponder@dasher.wustl.edu>
2022-10-25 13:17:46 -0500

    update promo header and logs to candidate 8.10.4 release

--- source/promo.f
+++ source/promo.f
@@ -30,7 +30,7 @@ c
      &        /,1x,'###',12x,'Tinker  ---  Software Tools for',
      &           ' Molecular Design',12x,'###',
      &        /,1x,'##',74x,'##',
-     &        /,1x,'##',24x,'Version 8.10.3   June 2022',24x,'##',
+     &        /,1x,'##',23x,'Version 8.10.4  October 2022',23x,'##',
      &        /,1x,'##',74x,'##',
      &        /,1x,'##',15x,'Copyright (c)  Jay William Ponder',
      &           '  1990-2022',15x,'##',
